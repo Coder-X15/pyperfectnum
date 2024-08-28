5f75361ca1e69670e824341d5b43056980c00722
Coder-X15 <samrubenabraham@yahoo.com>
2024-08-28 13:04:47 +0530

    Added all tests

--- src/main.py
+++ src/main.py
@@ -2,15 +2,19 @@
 
 import math
 
-def isPerfect(n : int) -> bool:
+
+def isPerfect(n: int) -> bool:
     try:
         proper_divisors = [i for i in range(1, n) if n % i == 0]
         return True if sum(proper_divisors) == n else False
     except TypeError:
         raise TypeError(f"Wrong argument type for testing perfectness:{type(n)}")
 
-def perfect_nums_in_range(n : int) -> list:
+
+def perfect_nums_in_range(n: int) -> list:
     try:
-        return [i for i in range(1, n+1) if isPerfect(i)]
+        if n < 0:
+            raise ValueError("Input should be a non-negative integer.")
+        return [i for i in range(1, n + 1) if isPerfect(i)]
     except TypeError:
         raise TypeError(f"Wrong argument type for function:{type(n)}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # tests for calculator
 
 import unittest
@@ -10,11 +10,18 @@ import pytest
 
 from src.main import isPerfect, perfect_nums_in_range
 
+
 class TestPerfectNum(unittest.TestCase):
 
     def test_isPerfect(self):
         self.assertEqual(isPerfect(6), True)
         self.assertEqual(isPerfect(28), True)
+
+    def test_perfect_nums_in_range(self):
+        self.assertEqual(perfect_nums_in_range(5), [])
+        self.assertEqual(perfect_nums_in_range(9), [6])
+        self.assertEqual(perfect_nums_in_range(30), [6, 28])
+
 
 def test_isPerfect():
     assert isPerfect(6) == True
@@ -36,12 +43,46 @@ def test_isPerfect():
         isPerfect()
 
 
-def test_addition_str_fail():
+def test_isPerfect_str_fail():
     with pytest.raises(TypeError):
         isPerfect("1")
     with pytest.raises(TypeError):
         isPerfect("13")
 
 
-if __name__ == '__main__':
-    unittest.main(argv=[''], verbosity=2, exit=False)
+def test_perfect_nums_in_range():
+    assert perfect_nums_in_range(1) == []
+    assert perfect_nums_in_range(5) == []
+    assert perfect_nums_in_range(10) == [6]
+    assert perfect_nums_in_range(14) == [6]
+    assert perfect_nums_in_range(27) == [6]
+    assert perfect_nums_in_range(28) == [6, 28]
+    assert perfect_nums_in_range(30) == [6, 28]
+    assert perfect_nums_in_range(500) == [6, 28, 496]
+
+    # test negative numbers
+    with pytest.raises(ValueError):
+        perfect_nums_in_range(-5)
+    with pytest.raises(ValueError):
+        perfect_nums_in_range(-10)
+    with pytest.raises(ValueError):
+        perfect_nums_in_range(-2)
+    # test float numbers
+    with pytest.raises(TypeError):
+        perfect_nums_in_range(1.1)
+    with pytest.raises(TypeError):
+        perfect_nums_in_range(19.12)
+    # test empty arguments
+    with pytest.raises(TypeError):
+        perfect_nums_in_range()
+
+
+def test_perfect_nums_in_range_str_fail():
+    with pytest.raises(TypeError):
+        perfect_nums_in_range("1")
+    with pytest.raises(TypeError):
+        perfect_nums_in_range("13")
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[""], verbosity=2, exit=False)
