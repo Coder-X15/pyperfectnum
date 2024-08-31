bd50fee7a5233980dc6deeac0024142b97b8fc64
Coder-X15 <samrubenabraham@gmail.com>
2024-08-31 12:43:37 +0530

    Refactored code, added more checks

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.main import is_perfect, perfect_nums_in_range
 
+
 class TestPerfectNum(unittest.TestCase):
     """
     Class for running tests
@@ -32,9 +33,10 @@ class TestPerfectNum(unittest.TestCase):
         self.assertEqual(perfect_nums_in_range(9), [6])
         self.assertEqual(perfect_nums_in_range(30), [6, 28])
 
+
 ####################### TESTS FOR IS_PERFECT() FUNCTION ################################
 def test_is_perfect_positive_int_pass():
-    """ 
+    """
     Testing the is_perfect() function for positive ints
     """
     assert is_perfect(6) is True
@@ -42,8 +44,9 @@ def test_is_perfect_positive_int_pass():
     assert not is_perfect(1)
     assert not is_perfect(3)
 
+
 def test_is_perfect_non_positive_int_pass():
-    """ 
+    """
     Testing the is_perfect() function for negative ints
     """
     assert not is_perfect(0)
@@ -52,30 +55,34 @@ def test_is_perfect_non_positive_int_pass():
     assert not is_perfect(-6)
     assert not is_perfect(-28)
 
+
 def test_is_perfect_positive_float_fail():
-    '''
+    """
     Testing the is_perfect() function for positive floats
-    '''
+    """
     with pytest.raises(TypeError):
         is_perfect(1.1)
     with pytest.raises(TypeError):
         is_perfect(19.12)
-        
+
+
 def test_is_perfect_negative_float_fail():
-    '''
+    """
     Testing the is_perfect() function for negative floats
-    '''
+    """
     with pytest.raises(TypeError):
         is_perfect(-3.14)
     with pytest.raises(TypeError):
         is_perfect(-6.28)
-    
+
+
 def test_is_perfect_empty_fail():
-    '''
+    """
     Testing the is_perfect() function for empty args
-    '''
+    """
     with pytest.raises(TypeError):
         is_perfect()
+
 
 def test_is_perfect_str_fail():
     """
@@ -85,6 +92,7 @@ def test_is_perfect_str_fail():
         is_perfect("1")
     with pytest.raises(TypeError):
         is_perfect("13")
+
 
 ####################### TESTS FOR PERFECT_NUMS_IN_RANGE() FUNCTION ################################
 def test_perfect_nums_in_range_positive_int_pass():
@@ -96,6 +104,7 @@ def test_perfect_nums_in_range_positive_int_pass():
     assert perfect_nums_in_range(10) == [6]
     assert perfect_nums_in_range(28) == [6, 28]
     assert perfect_nums_in_range(500) == [6, 28, 496]
+
 
 def test_perfect_nums_in_range_non_positive_int_fail():
     """
@@ -110,6 +119,7 @@ def test_perfect_nums_in_range_non_positive_int_fail():
     with pytest.raises(ValueError):
         perfect_nums_in_range(-2)
 
+
 def test_perfect_nums_in_range_positive_float_fail():
     """
     Tests for the  perfect_nums_in_range() function for positive ints
@@ -119,6 +129,7 @@ def test_perfect_nums_in_range_positive_float_fail():
     with pytest.raises(TypeError):
         perfect_nums_in_range(19.12)
 
+
 def test_perfect_nums_in_range_negative_float_fail():
     """
     Tests for the  perfect_nums_in_range() function for positive ints
@@ -127,7 +138,8 @@ def test_perfect_nums_in_range_negative_float_fail():
         perfect_nums_in_range(-3.14)
     with pytest.raises(TypeError):
         perfect_nums_in_range(-6.28)
-    
+
+
 def test_perfect_nums_in_range_empty_fail():
     """
     Tests for the  perfect_nums_in_range() function for empty args
