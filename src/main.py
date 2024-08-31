464aff6e44bf935e6a8a4277571194635135087e
Coder-X15 <samrubenabraham@gmail.com>
2024-08-31 12:39:57 +0530

    Refactored code, added more checks

--- src/main.py
+++ src/main.py
@@ -4,10 +4,29 @@
 ## EDIT : I just removed an unnecessary import
 def is_perfect(n: int) -> bool:
     """
-    Returns true if n is a perfect number
+    Args:
+        n : a positive integer
+    Returns:
+        Returns True if n is a perfect number (i.e., a number that is equal to the sum of its proper divisors) else False
+        (NB: The same returns False even when n is less than or equal to 0)
+    Raises:
+        TypeError: if  n is not an integer
+    Examples:
+        >>>  is_perfect(6) # 6 is a perfect number
+        True
+        >>> is_perfect(9) # 9 is not a perfect number
+        False
+        >>> is_perfect(1.2) # wrong type of argument
+        Traceback (most exception):
+        ...
+        TypeError: Wrong argument type for testing perfectness: <class 'float'>
+        >>> is_perfect("10") # wrong type of argument
+        Traceback (most exception):
+        ...
+        TypeError: Wrong argument type for testing perfectness: <class 'str'>
     """
     try:
-        if n < 0:
+        if n <= 0 and type(n) == int:
             return False
         proper_divisors = [i for i in range(1, n) if n % i == 0]
         return True if sum(proper_divisors) == n else False
@@ -17,11 +36,34 @@ def is_perfect(n: int) -> bool:
 
 def perfect_nums_in_range(n: int) -> list:
     """
-    Returns  a list of perfect numbers in the range [1, n]
+    Args:
+        n : a positive integer
+    Returns:
+        Returns a list of perfect numbers in the list [i for i in range(1, n+1)]
+    Raises:
+        TypeError: if  n is not an integer
+        ValueError: if n is not a positive integer
+    Examples:
+        >>>  perfect_nums_in_range(6)
+        [6]
+        >>> perfect_nums_in_range(9)
+        [6]
+        >>> perfect_nums_in_range(1.2) # wrong type of argument
+        Traceback (most exception):
+        ...
+        TypeError: Wrong argument type for function: <class 'float'>
+        >>> perfect_nums_in_range("12") # wrong type of argument
+        Traceback (most exception):
+        ...
+        TypeError: Wrong argument type for function: <class 'str'>
+        >>> perfect_nums_in_range(-8) # wrong kind of argument
+        Traceback (most exception):
+        ...
+        ValueError: Input should be a positive integer
     """
     try:
-        if n < 0:
-            raise ValueError("Input should be a non-negative integer.")
+        if n <= 0 and type(n) == int:
+            raise ValueError("Input should be a positive integer.")
         return [i for i in range(1, n + 1) if is_perfect(i)]
     except TypeError:
         raise TypeError(f"Wrong argument type for function:{type(n)}")
